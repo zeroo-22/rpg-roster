ac2f7232ef35a5a8a6aa134c049b0da4b1bd736d
Bolty207 <johnjedf@gmail.com>
2026-05-25 19:47:35 +0800

    Finalterm

--- DATABASE_engineer.h
+++ DATABASE_engineer.h
@@ -10,12 +10,17 @@ struct Character {
     int id;
     std::string name;
     std::string role;
+    int damage;
+    int health;
+    int defense;
 };
 
 class RosterDB {
 private:
     std::vector<Character> roster;
     int nextId = 1;
+
+    Character assignStats(const std::string& name, const std::string& role);
 
 public:
     // Create
@@ -29,6 +34,9 @@ public:
 
     // Delete
     bool removeCharacter(int id);
+
+    // Save
+    void saveToCSV(const std::string& filename) const;
 };
 
 #endif // DATABASE_ENGINEER_H
--- system_architecture.h
+++ system_architecture.h
@@ -9,14 +9,3 @@ std::string createHeroName(std::vector<std::string>& heroes);
 void battleHeroes(std::vector<std::string>& heroes);
 
 #endif
-#ifndef SYSTEM_ARCHITECTURE_H
-#define SYSTEM_ARCHITECTURE_H
-
-#include <string>
-#include <vector>
-
-// Function declarations
-std::string createHeroName(std::vector<std::string>& heroes);
-void battleHeroes(std::vector<std::string>& heroes);
-
-#endif
